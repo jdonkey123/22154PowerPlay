863a2e8a7c6ece4c3cfdf1aebd2eb19cfa26cefd
Aaron Vronko <avronko@gmail.com>
2022-10-09 14:57:58 -0400

    Created copy of robot class for AJV to test code

---
+++ Robot_AJV.java
@@ -0,0 +1,34 @@
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Robot_AJV {
+
+    RobotHardware robotHardware = new RobotHardware();
+
+    public void InitHardware(HardwareMap ahw){
+        robotHardware.Init(ahw);
+    }
+
+    public void LeftDriveMotor(double power){
+        robotHardware.leftMotor.setPower(power);
+    }
+
+    public void RightDriveMotor(double power){
+        robotHardware.rightMotor.setPower(power);
+    }
+
+    public void Drive(double lP, double rP){
+        robotHardware.leftMotor.setPower(lP);
+        robotHardware.rightMotor.setPower(rP);
+    }
+
+    public void armMotor(double power){
+        robotHardware.armMotor.setPower(power);
+    }
+    public void closedServo(){
+        robotHardware.grabservo.setPosition(0);
+
+    }
+    public void openServo(){
+        robotHardware.grabservo.setPosition(1);
+    }
+}
